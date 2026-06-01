21bd09493fe33c5a79522e351e6827a339648372
ahmedeid2260 <a.email2260@gmail.com>
2026-06-01 14:30:05 +0300

    feat: replace App component with RouterProvider for routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ROUTES } from './utils/routes.tsx';
 // import './index.css'
-import App from './App.tsx'
 
+const router = createBrowserRouter(ROUTES);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
