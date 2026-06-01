fe8f36b4fea590310667b0dac34193b05be8b81a
ahmedeid2260 <a.email2260@gmail.com>
2026-06-01 14:30:13 +0300

    feat: add initial routing setup with ROUTES constant

---
+++ src/utils/routes.tsx
@@ -0,0 +1,13 @@
+import App from "../App";
+
+export const ROUTES = [
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <App />,
+    },
+    // {
+    //     path: '*',
+    //     element: <h1>404 Not Found</h1>,
+    // }
+];
