1d661e8bd7578c0d6ffe3b4cc4826ca4663dabe1
ahmedeid2260 <a.email2260@gmail.com>
2026-06-03 14:02:31 +0300

    feat: add FontAwesome icons library for enhanced UI components

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ROUTES } from './utils/routes.tsx';
 // import './index.css'
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fas, far, fab);
 const router = createBrowserRouter(ROUTES);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
