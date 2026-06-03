84ebc81afee153d87e941c0c9b8121b9d36843f2
ahmedeid2260 <a.email2260@gmail.com>
2026-06-03 16:02:46 +0300

    feat: update ListCouches component to use ListWrapper and MainTable for improved layout

--- src/pages/couches/ListCouches.tsx
+++ src/pages/couches/ListCouches.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import MainTable from '../../components/Table'
+import ListWrapper from '../../components/wrappers/ListWrapper'
 
 export default function ListCouches() {
     return (
-        <div>ListCouches</div>
+        <ListWrapper>
+            <MainTable />
+        </ListWrapper>
     )
 }
