1bf3d5d4e3c13fc7e0efabbb07399caf89e56c7e
ahmedeid2260 <a.email2260@gmail.com>
2026-06-04 16:12:30 +0300

    feat: implement MainTable component with dynamic headers and pagination support

---
+++ src/components/tables/Table.tsx
@@ -0,0 +1,89 @@
+import {
+    Box,
+    TableFooter as MuiTableFooter,
+    Table,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from '@mui/material';
+import Skeleton from '../skeleton';
+
+
+
+
+export default function MainTable({
+    TableHeaderArray,
+    TableBody,
+    TablePagination,
+}: {
+    TableHeaderArray?: any[];
+    TableBody?: React.ReactNode;
+    TablePagination?: React.ReactNode;
+}) {
+
+    return (
+        <Box sx={{
+            borderRadius: 0,
+            width: "100%",
+            display: "grid",
+            height: "100%",
+            alignContent: "space-between",
+            gridTemplateRows: "auto 1fr auto",
+            flex: 1,
+            overflow: "hidden",
+        }}>
+            <TableContainer
+                sx={{
+                    width: "100%",
+                    height: "100%",
+                    overflow: "auto",
+                }}
+                id="floating-head-table-container"
+            >
+                <Table
+                    aria-label="simple table">
+                    <TableHead>
+                        <TableRow sx={{
+                            "& > *": {
+                                fontWeight: "bold",
+                                position: "sticky",
+                                top: 0,
+                                zIndex: 10,
+                                backgroundColor: "#f5f5f5",
+                                textTransform: "capitalize",
+                            }
+                        }}>
+                            {TableHeaderArray?.map(({ name, className }, index) => (
+                                <TableCell
+                                    sx={{
+                                        fontWeight: "bold",
+                                    }}
+                                    // align="right"
+                                    key={index}
+                                    className={className}
+                                >
+                                    {name}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <Skeleton />
+                    {TableBody && TableBody}
+                    <MuiTableFooter>
+                        <TableRow sx={{
+                            position: "sticky",
+                            bottom: 0,
+                            backgroundColor: "#f5f5f5",
+                            zIndex: 9,
+                        }}>
+                            <TableCell colSpan={60} sx={{ padding: 0 }}>
+                                {TablePagination && TablePagination}
+                            </TableCell>
+                        </TableRow>
+                    </MuiTableFooter>
+                </Table>
+            </TableContainer >
+        </Box>
+    );
+}
