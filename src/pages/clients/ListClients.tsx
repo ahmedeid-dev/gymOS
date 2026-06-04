d9b0468393a0bc48cf25bdba61703a0947ac9425
ahmedeid2260 <a.email2260@gmail.com>
2026-06-04 16:12:15 +0300

    feat: enhance ListClients component with dynamic table headers and pagination

--- src/pages/clients/ListClients.tsx
+++ src/pages/clients/ListClients.tsx
@@ -1,10 +1,38 @@
-import MainTable from '../../components/Table'
-import ListWrapper from '../../components/wrappers/ListWrapper'
+import { useMemo } from 'react';
+import MainTable from '../../components/tables/Table';
+import ListWrapper from '../../components/wrappers/ListWrapper';
+import TablePagination from '../../components/tables/TablePagination';
 
 export default function ListClients() {
+    const ListClientsArray = useMemo(() => ([
+        {
+            name: "Dessert (100g serving)",
+            className: "FH",
+        },
+        {
+            name: "Calories",
+        },
+        {
+            name: "Fat (g)",
+        },
+        {
+            name: "Carbs (g)",
+        },
+        {
+            name: "Protein (g)",
+            className: "LH",
+        },
+        // ! add more
+    ]), []);
+    const ListCouchesPagination = useMemo(() => (
+        <TablePagination />
+    ), []);
     return (
         <ListWrapper>
-            <MainTable />
+            <MainTable
+                TableHeaderArray={ListClientsArray}
+                TablePagination={ListCouchesPagination}
+            />
         </ListWrapper>
     )
 }
