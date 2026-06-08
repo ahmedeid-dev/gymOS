88ded0cff0e2e8dd26602698ce6964032065329a
ahmedeid2260 <a.email2260@gmail.com>
2026-06-08 16:11:18 +0300

    feat: add Footer component for copyright information

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+export default function Footer() {
+    return (
+        <div
+            style={{
+                margin: "auto",
+            }}
+        >
+            Copyright © 2026 GymOS. All rights reserved.
+        </div>
+    )
+}
